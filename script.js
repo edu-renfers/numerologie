2d81adfee785a64fcf0858b86787d1edacc0ad2d
edu-renfers <edu-renfers@eduge.ch>
2020-03-10 14:40:37 +0000

    fonctions de base implémentées

--- script.js
+++ script.js
@@ -16,21 +16,24 @@ function carReduction (car) {
   //l'index du caractère retourne son nombre qu'il faut réduire par la suite (espace = 0)
   var retour = 0
   // il faut mettre le caractère en majuscule, puis trouver son index (position) dans alphabet, puis réduire ce nombre et le retourner
-
-  if () {
-
+  let pos = alphabet.indexOf(car.toUpperCase())
+  if (pos>0) {
+    retour = pos
   }
-  return retour
+  return reduction(retour)
 }
-
 // "motReduction" transforme un mot en un nombre réduit
 
 function motReduction(mot){
   var retour = 0
   // parcourir tous les caractères du mot et les réduire avec carReduction et en faire la somme, qu'on réduit et qu'on retourne
-
-  return retour
+  var eclate = Array.from(mot)
+  eclate.forEach(function (element){
+    retour += carReduction(element)
+  })
+  return reduction(retour)
 }
+console.log(motReduction('Serge Alexandre'))
 
 // "expression" calcule le nombre d'expression qui est la réduction (en un chiffre) des prénoms et nom de famille
 
@@ -60,22 +63,38 @@ function hereditaire(nom){
 
 function realisation(nom){
   var retour = 0
-
-  return retour
+  const voyelles = "AEIOUY"
+  var eclate = Array.from(nom)
+  eclate.forEach(function (element){
+    if (!voyelles.indexOf(element)) retour += carReduction(element)
+  })
+  return reduction(retour)
 }
+console.log(realisation('Serge Alexandre Renfer'))
 
 // "intime" est la réduction des voyelles du nom complet
 
 function intime(nom){
   var retour = 0
-
-  return retour
+  const voyelles = "AEIOUY"
+  var eclate = Array.from(nom)
+  eclate.forEach(function (element){
+    if (voyelles.indexOf(element)) retour += carReduction(element)
+  })
+  return reduction(retour)
 }
+console.log(intime('Serge Alexandre Renfer'))
 
 // "vie" ou chemin de vie est la réduction de la date de naissance
 
 function vie(naissance){
   var retour = 0
-
-  return retour
+  const chiffres = "0123456789"
+  var eclate = Array.from(naissance)
+  eclate.forEach(function (element){
+    let pos = chiffres.indexOf(element)
+    if (pos>0) retour += pos
+  })
+  return reduction(retour)
 }
+console.log(vie('20/10/1960'))
